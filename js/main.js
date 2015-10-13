784d2bf6f4520f520449b417a2d98b1def0f21b2
Xwatson <xuwusheng@hotmail.com>
2015-10-13 17:57:29 +0800

    入口

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xuwusheng on 15/8/15.
+ */
+define(['angular','./app'], function (angular) {
+    'use strict';
+    angular.element(document).ready(function () {
+        angular.bootstrap(document,['starter']);
+    });
+
+});
